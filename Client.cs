37423eea41316743ace5082d5e89a050130a8e51
Edward <elp49@drexel.edu>
2019-03-10 22:17:23 -0400

    combined client classes

--- Client.cs
+++ Client.cs
@@ -4,6 +4,18 @@ namespace AlphaMobile
 {
 	abstract class Client
 	{
-		//public static string Location { get; set; }
+		public static void Main(string[] args) {
+			BaseSmartphoneFactory smartphoneFactorySG = BaseSmartphoneFactory.GetSmartphoneFactory("Singapore");
+			Smartphone smart4SG = smartphoneFactorySG.CreateSmartphone("smart4", "AlphaMobile Smart4", 499.99);
+			Console.WriteLine();
+			Smartphone smart5SG = smartphoneFactorySG.CreateSmartphone("smart5", "AlphaMobile Smart5", 599.99);
+			Console.WriteLine();
+
+			BaseSmartphoneFactory smartphoneFactoryNY = BaseSmartphoneFactory.GetSmartphoneFactory("New York");
+			Smartphone smart5NY = smartphoneFactoryNY.CreateSmartphone("smart5", "AlphaMobile Smart5", 599.99);
+			Console.WriteLine();
+			Smartphone smart6NY = smartphoneFactoryNY.CreateSmartphone("smart6", "AlphaMobile Smart6", 699.99);
+			Console.WriteLine();
+		}
 	}
 }
